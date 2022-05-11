593acfb5c12e2ca6ec146ec3d3adeaadb864c68e
snehal384 <104683010+snehal384@users.noreply.github.com>
2022-05-11 22:16:10 +0530

    Create range.py

---
+++ range.py
@@ -0,0 +1,13 @@
+#program to print all positive number in range  
+list1=[12,-7,5,64,-14]
+for num in list1:
+      if num>=1:
+         print(num,end=" ")
+print("for list1")
+list2=[12,14,-94,3]
+for num in list2:
+      if num>=1;
+         print(num,end=" ")
+print("for list2")
+print("done")
+         
